4def321b9a9aa0997f6a6dcc1b6634388eb75585
rox38431 <jojo@cml16.csie.ntu.edu.tw>
2020-04-09 08:28:23 +0800

    修改 models 最終的輸出 class 數為 2

--- models/resnet.py
+++ models/resnet.py
@@ -76,7 +76,7 @@ class ResNet(nn.Module):
         self.layer2 = self._make_layer(block, 128, num_blocks[1], stride=2)
         self.layer3 = self._make_layer(block, 256, num_blocks[2], stride=2)
         self.layer4 = self._make_layer(block, 512, num_blocks[3], stride=2)
-        self.linear = nn.Linear(32768, num_classes)
+        self.linear = nn.Linear(8192, num_classes)
 
     def _make_layer(self, block, planes, num_blocks, stride):
         strides = [stride] + [1]*(num_blocks-1)
